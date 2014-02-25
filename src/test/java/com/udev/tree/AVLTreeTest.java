e1f716cfd3f78ba35c57af04b6d30a846de75e64
Oleg Krupenya <oleg.krupenya@akvelon.com>
2014-02-25 22:18:37 +0200

    tests for deleting

--- src/test/java/com/udev/tree/AVLTreeTest.java
+++ src/test/java/com/udev/tree/AVLTreeTest.java
@@ -52,6 +52,40 @@ public class AVLTreeTest {
     }
 
     @Test
+    public void deleteTest_hp() {
+        this.tree.add(8);
+        this.tree.add(3);
+        this.tree.add(10);
+        this.tree.add(6);
+        this.tree.add(1);
+        this.tree.add(5);
+        this.tree.add(7);
+        this.tree.add(14);
+        this.tree.add(13);
+
+        Integer root = this.tree.delete(8);
+        Integer left = this.tree.delete(3);
+        Integer right = this.tree.delete(10);
+        Integer leftRight = this.tree.delete(6);
+        Integer leftLeft = this.tree.delete(1);
+        Integer leftRightLeft = this.tree.delete(5);
+        Integer leftRightRight = this.tree.delete(7);
+        Integer rightRight = this.tree.delete(14);
+        Integer rightRightLeft = this.tree.delete(13);
+
+        Assert.assertEquals(0, this.tree.size());
+        Assert.assertFalse(this.tree.contains(root));
+        Assert.assertFalse(this.tree.contains(left));
+        Assert.assertFalse(this.tree.contains(right));
+        Assert.assertFalse(this.tree.contains(leftRight));
+        Assert.assertFalse(this.tree.contains(leftLeft));
+        Assert.assertFalse(this.tree.contains(leftRightLeft));
+        Assert.assertFalse(this.tree.contains(leftRightRight));
+        Assert.assertFalse(this.tree.contains(rightRight));
+        Assert.assertFalse(this.tree.contains(rightRightLeft));
+    }
+
+    @Test
     public void leftLeftCaseTest() {
         this.tree.add(50);
         this.tree.add(30);
